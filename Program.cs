b70fce83b4fdef2a82122a3f5ee1185898b3fe73
Shilov Ilia <ilyashilov@bk.ru>
2023-03-26 22:56:34 +0300

    added programm

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+
+string[] StringArray3Symol(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] resultarray = new string[count];
+    count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            resultarray[count] = array[i];
+            count++;
+        }
+    }
+    return resultarray;
+}
+
+void ShowStringArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[" + array[i] + "]");
+    }
+}
+
+ShowStringArray(StringArray3Symol(array));
